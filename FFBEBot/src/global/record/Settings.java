b830cd0b818825551d221e0fbddbee3e707cf750
Allen <chonglun.chen@gmail.com>
2017-11-03 00:31:03 -0400

    Literally changing a variable
    -update for new banner, bit early

--- FFBEBot/src/global/record/Settings.java
+++ FFBEBot/src/global/record/Settings.java
@@ -18,7 +18,7 @@ import XML.Elements;
  */
 public class Settings {
 	//general static settings, stored here to change easily
-	public static final String token=Secrets.testToken;
+	public static final String token=Secrets.token;
 	public static final String prefix="-!";
 	public static final String modPrefix="~!";
 	public static final String UA="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
@@ -38,7 +38,7 @@ public class Settings {
 	public static HashSet<String> blacklist=new HashSet<String>();
 	public static long dailyTime;
 	public static HashMap<String,Settings> guilds=new HashMap<String,Settings>();//map of guilds and settings stored locally for easy access	
-	public static Banner DefaultBanner=Banner.HalloweenP2_1;//default banner used by summon simulators
+	public static Banner DefaultBanner=Banner.Loren;//default banner used by summon simulators
 	
 	//guild settings
 	public String guildPrefix="";
