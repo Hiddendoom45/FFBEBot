ae56fa13a137468cb1104192005c5f42f6d5d582
Allen <chonglun.chen@gmail.com>
2018-09-23 00:45:10 -0400

    Update UnitArt
    -use embeds to avoid ugly links

--- FFBEBot/src/commands/UnitArt.java
+++ FFBEBot/src/commands/UnitArt.java
@@ -1,9 +1,13 @@
 package commands;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import global.record.Log;
 import global.record.SaveSystem;
+import net.dv8tion.jda.core.entities.MessageEmbed.Field;
+import net.dv8tion.jda.core.entities.MessageEmbed.ImageInfo;
+import net.dv8tion.jda.core.entities.impl.MessageEmbedImpl;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import util.Lib;
 import util.Selection;
@@ -13,13 +17,27 @@ import util.unit.UnitOverview;
 public class UnitArt extends UnitSelection implements Command,Selection  {
 	public static void sendImage(UnitOverview Ounits, int rarity,MessageReceivedEvent event,int index){
 		if(rarity==0){
-			Lib.sendMessage(event, ":art:"+Ounits.getNames().get(index)+"\n"+Ounits.getData(index).imgUrl);
+			MessageEmbedImpl embed=new MessageEmbedImpl();
+			String url = Ounits.getData(index).imgUrl;
+			embed.setImage(new ImageInfo(url, url, 500, 500));
+			ArrayList<Field> fields = new ArrayList<Field>();
+			fields.add(new Field(":art:"+Ounits.getNames().get(index)+" "+Lib.extractNumber(Ounits.getData(index).maxR)+"★", "[link to image]("+url+")", false));
+			System.out.println(Ounits.getData(index).maxR+" "+Ounits.getData(index).baseR);
+			embed.setFields(fields);
+			event.getChannel().sendMessage(embed).complete();
 		}
 		else{
 			UnitInfo info;
 			try {
 				info = new UnitInfo(Ounits.getData(index).unitUrl);
-				Lib.sendMessage(event, (rarity<=info.maxRarity&&rarity>=info.minRarity?":art:"+info.unitName+"\n"+info.sprites[rarity-info.minRarity]:"Unit does not have this rarity"));
+				MessageEmbedImpl embed=new MessageEmbedImpl();
+				String url = (rarity<=info.maxRarity&&rarity>=info.minRarity?info.sprites[rarity-info.minRarity]:"https://d1u5p3l4wpay3k.cloudfront.net/exvius_gamepedia_en/b/b9/Icon-Crimson_Gem.png?version=20c46d237943d81039478b494150c0d8");
+				System.out.println(url);
+				embed.setImage(new ImageInfo(url, url, 500, 500));
+				ArrayList<Field> fields = new ArrayList<Field>();
+				fields.add(new Field(":art:"+info.unitName+" "+rarity+"★", "[link to image]("+url+")", false));
+				embed.setFields(fields);
+				event.getChannel().sendMessage(embed).complete();
 			} catch (IOException e) {
 				Log.logError(e);
 			}
