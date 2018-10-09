d2d29e85fbeb27f86e00c5d17781e9626b3fd809
Allen <chonglun.chen@gmail.com>
2018-10-09 17:42:26 -0400

    making updating Unit more easy
    -added option to split based on upgrades stuffs

--- FFBEBot/src/commands/overide/UnitEPrint.java
+++ FFBEBot/src/commands/overide/UnitEPrint.java
@@ -16,20 +16,17 @@ public class UnitEPrint extends OverrideGenerics implements OverrideCommand {
 	public void action(HashMap<String, String[]> args, MessageReceivedEvent event) {
 		UnitOverview Ounit = new UnitOverview();
 		String s="";
+		int upgrade = 0;
+		if(args.containsKey("upgrade")){
+			upgrade = Integer.parseInt(args.get("upgrade")[0]);
+		}
 		if(args.containsKey("i")){
 			for(String ind:args.get("i")){
 				try{
 					unitData d = Ounit.getData(Integer.parseInt(ind));
 					UnitInfo ui = SaveSystem.getExviusUnit(d.name);
 					//build a line of the string
-					s+=d.name+"(\""+d.name+"\",new String[]{";
-					for(int c =0;c<ui.sprites.length;c++){
-						s+="\""+ui.sprites[c].substring(Settings.ExvicusIMGURL.length())+"\"";
-						if(c!=ui.sprites.length-1){
-							s+=",";
-						}
-					}
-					s+="},"+ui.minRarity+"),\n";
+					s+=buildString(d,ui,upgrade);
 				}catch(NumberFormatException e){};
 			}
 		}
@@ -39,20 +36,35 @@ public class UnitEPrint extends OverrideGenerics implements OverrideCommand {
 					unitData d = Ounit.getData(i);
 					//build line
 					UnitInfo ui = SaveSystem.getExviusUnit(d.name);
-					s+=d.name+"(\""+d.name+"\",new String[]{";//first part with name
-					for(int c =0;c<ui.sprites.length;c++){//second part with urls
-						s+="\""+ui.sprites[c].substring(Settings.ExvicusIMGURL.length())+"\"";
-						if(c!=ui.sprites.length-1){
-							s+=",";
-						}
-					}
-					s+="},"+ui.minRarity+"),\n";//last part wth base rarity
+					s+=buildString(d,ui,upgrade);
 				}
 			}catch(NumberFormatException | IndexOutOfBoundsException e){}
 		}
-
 		Lib.sendMessage(event, s);
-
+	}
+	private static String buildString(unitData d,UnitInfo ui,int upgrade){
+		String s = "";
+		s+=d.name+"(\""+d.name+"\",new String[]{";//first part with name
+		for(int c =0;c<(upgrade>0?upgrade-ui.minRarity:ui.sprites.length);c++){//second part with urls
+			s+="\""+ui.sprites[c].substring(Settings.ExvicusIMGURL.length())+"\"";
+			if(c!=ui.sprites.length-upgrade-1){
+				s+=",";
+			}
+		}
+		
+		s+="},"+ui.minRarity+")";//last part wth base rarity
+		if(upgrade>0){
+			s+="\n\t@Override\n\tpublic void setup(){\n\t\t\tsetUpgrades(new String[]{";
+			for(int c = upgrade-ui.minRarity;c<ui.sprites.length;c++){
+				s+="\""+ui.sprites[c].substring(Settings.ExvicusIMGURL.length())+"\"";
+				if(c!=ui.sprites.length-1){
+					s+=",";
+				}
+			}
+			s+="});\n\t\t}\n\t}";
+		}
+		s+=",\n";
+		return s;
 	}
 
 	@Override
