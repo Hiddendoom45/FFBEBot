987daec8e6edec3187d9a19dc564c4e159d533f9
Allen <chonglun.chen@gmail.com>
2018-10-17 14:17:53 -0400

    Update Unit command
    -completely embed now like runit
    -changed color to blue the generic color of FFBE (probably)

--- FFBEBot/src/commands/Units.java
+++ FFBEBot/src/commands/Units.java
@@ -13,31 +13,28 @@ import util.unit.UnitOverview;
 
 public class Units extends UnitSelection implements Command,Selection {
 	public void sendUnitData(UnitInfo info,MessageReceivedEvent event){
-		String out="";
-		out+="__**"+info.unitName+"**__\t";
-		out+="Rarity:"+info.minRarity+"★ - "+info.maxRarity+"★\t";
-		out+="Role:"+info.role+"\t";
-		out+="\n__Stats__";
+		String stats="";
 		for(int i=0;i<info.stats.stats.length;i++){
-			out+="\n"+addStars(info.stats.stats[i].rarity);
-			out+="**HP**:"+Lib.pad(info.stats.stats[i].HP, 6)+Lib.pad("("+info.statIncrease.stats[i].HP+")", 6);
-			out+="**MP**:"+Lib.pad(info.stats.stats[i].MP, 6)+Lib.pad("("+info.statIncrease.stats[i].MP+")", 6);
-			out+="**ATK**:"+Lib.pad(info.stats.stats[i].ATK, 6)+Lib.pad("("+info.statIncrease.stats[i].ATK+")", 6);
-			out+="**DEF**:"+Lib.pad(info.stats.stats[i].DEF, 6)+Lib.pad("("+info.statIncrease.stats[i].DEF+")", 6);
-			out+="**MAG**:"+Lib.pad(info.stats.stats[i].MAG, 6)+Lib.pad("("+info.statIncrease.stats[i].MAG+")", 6);
-			out+="**SPR**:"+Lib.pad(info.stats.stats[i].SPR, 6)+Lib.pad("("+info.statIncrease.stats[i].SPR+")", 6);
+			stats+="\n"+addStars(info.stats.stats[i].rarity);
+			stats+="**HP**:"+Lib.pad(info.stats.stats[i].HP, 6)+Lib.pad("("+info.statIncrease.stats[i].HP+")", 6);
+			stats+="**MP**:"+Lib.pad(info.stats.stats[i].MP, 6)+Lib.pad("("+info.statIncrease.stats[i].MP+")", 6);
+			stats+="**ATK**:"+Lib.pad(info.stats.stats[i].ATK, 6)+Lib.pad("("+info.statIncrease.stats[i].ATK+")", 6);
+			stats+="**DEF**:"+Lib.pad(info.stats.stats[i].DEF, 6)+Lib.pad("("+info.statIncrease.stats[i].DEF+")", 6);
+			stats+="**MAG**:"+Lib.pad(info.stats.stats[i].MAG, 6)+Lib.pad("("+info.statIncrease.stats[i].MAG+")", 6);
+			stats+="**SPR**:"+Lib.pad(info.stats.stats[i].SPR, 6)+Lib.pad("("+info.statIncrease.stats[i].SPR+")", 6);
 		}
 		EmbedBuilder embed=new EmbedBuilder();
-		embed.setAuthor(info.unitName,info.URL);
-		embed.setDescription("[unit link]("+info.URL+")");
+		embed.setAuthor(info.unitName+"    Rarity:"+info.minRarity+"★ - "+info.maxRarity+"★\t",info.URL);
+		embed.setDescription("Role:"+info.role+"\n"+"[unit link]("+info.URL+")");
 		if(!(info.trustLink==null)){
 			embed.addField(new Field("TM - "+info.trustName,"[TM link]("+info.trustLink+")\n"+info.trustDetails,false));
 		}
 		if(!(info.trustLink==null)){
 			embed.addField(new Field("STM - "+info.sTrustName,"[STM link]("+info.sTrustLink+")\n"+info.sTrustDetails,false));
 		}
+		embed.setColor(2584805);
+		embed.addField(new Field("Stats",stats,false));
 		embed.setImage(info.imgOverviewURL);
-		Lib.sendMessage(event, out);
 		Lib.sendEmbed(event, embed);
 	}
 	public String addStars(String s){
