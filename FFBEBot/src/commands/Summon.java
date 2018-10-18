0e277c839976744dbd78124d564311b4ec1d705e
Allen <chonglun.chen@gmail.com>
2018-10-18 11:27:42 -0400

    NullPointerException e

--- FFBEBot/src/commands/Summon.java
+++ FFBEBot/src/commands/Summon.java
@@ -44,9 +44,9 @@ public class Summon extends CommandGenerics implements Command {
 						if(num==11){
 							sendImage(event, Pull.pull11(pullBanner),pullBanner.name);
 						}
-						else if(num==666&&Unit.valueOf("Lucifer")!=null){
+						else if(num==666&&Unit.valueOf("Lucius")!=null){
 							ArrayList<UnitSpecific> units = new ArrayList<UnitSpecific>();
-							units.add(new UnitSpecific(Unit.valueOf("Lucifer"),5));
+							units.add(new UnitSpecific(Unit.valueOf("Lucius"),5));
 							sendImage(event,units,pullBanner.name);
 						}
 						else if(num==0){
