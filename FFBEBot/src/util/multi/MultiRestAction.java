60ff858041dec540cb0932309584cdd4ab67cf9d
Allen <chonglun.chen@gmail.com>
2018-11-21 08:58:28 -0500

    Add handler for multiRestAction
    -so submit/queue can be used in multiMessage

---
+++ FFBEBot/src/util/multi/MultiRestAction.java
@@ -0,0 +1,58 @@
+package util.multi;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.requests.RequestFuture;
+import net.dv8tion.jda.core.requests.RestAction;
+
+/**
+ * Sort of janky way of merging multiple RestActions, 
+ * uses extends in order to act masquade as a rest action
+ * 
+ * Follows the same principles of MultiMessage
+ * 
+ * <ul>
+ * <li>If it is a singleton value that cannot be joined it will return the value
+ * of the last action</li>
+ * <li>If it is something that can be joined it will join the values from the
+ * first to the last action</li>
+ * <li>Clear/reset actions will generally affect all actions</li>
+ * </ul>
+ * 
+ * @author Allen
+ *
+ * @param <T>
+ */
+public abstract class MultiRestAction<T> extends RestAction<T>{
+	private final RestAction<T>[] actions;
+	public MultiRestAction(JDA jda, RestAction<T>[] actions){
+		super(jda, null);//pass null checks
+		this.actions = actions;
+	}
+	@Override
+	public RestAction<T> setCheck(BooleanSupplier checks){
+		for(RestAction<T> action : actions){
+			action.setCheck(checks);
+		}
+		return this;
+	}
+	//overrides all other queue methods as they all call this method in superclass
+	@Override
+	public void queue(Consumer<? super T> success, Consumer<? super Throwable> failure){
+		for(RestAction<T> action : actions){
+			action.queue(success,failure);
+		}
+	}
+	@Override
+	public RequestFuture<T> submit(boolean shouldQueue){
+		for(RestAction<T> action:actions){
+			action.submit(shouldQueue);
+		}
+		return null;//not going to use this for now so this will work
+	}
+	
+
+
+}
