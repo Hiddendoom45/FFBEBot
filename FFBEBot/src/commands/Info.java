1cd4c2cc03e142b80c4db97a00955264f89774e6
Allen <chonglun.chen@gmail.com>
2018-11-20 20:12:33 -0500

    Improved info command a bit
    -display time zone of bot
    -make clearer that it uses 2^20 as base for MB

--- FFBEBot/src/commands/Info.java
+++ FFBEBot/src/commands/Info.java
@@ -4,7 +4,9 @@ package commands;
 import java.awt.Color;
 import java.text.SimpleDateFormat;
 import java.time.OffsetDateTime;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 import global.Main;
 import global.record.Settings;
@@ -26,8 +28,8 @@ public class Info extends CommandGenerics implements Command{
 		embed.addField(new Field("Servers", "Serving on "+Main.jda.getGuilds().size()+" servers", true));
 		embed.addField(new Field("Channels","Serving on "+Main.jda.getTextChannels().size()+" channels",true));
 		embed.addField(new Field("User","Serving "+Main.jda.getUsers().size()+" users",false));
-		embed.addField(new Field("Last Launch",""+new SimpleDateFormat("[yyyy-MM-dd-HH:mm]").format(new Date(Settings.ID)),true));
-		embed.addField(new Field("Memory",(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/1048576+"MB/"+Runtime.getRuntime().totalMemory()/1048576+"MB",true));
+		embed.addField(new Field("Last Launch",new SimpleDateFormat("[yyyy-MM-dd-HH:mm]").format(new Date(Settings.ID))+" "+Calendar.getInstance().getTimeZone().getDisplayName(false, TimeZone.SHORT),true));
+		embed.addField(new Field("Memory",(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/(1024*1024)+"MB/"+Runtime.getRuntime().totalMemory()/(1024*1024)+"MB",true));
 		embed.addField(new Field("Credits","All data on the game taken from [the exvius wiki](https://exvius.gamepedia.com/) and [the reddit wiki](https://reddit.com/r/FFBraveExvius/)",true));
 		embed.setColor(Color.GREEN);
 		embed.setDescription("general info");
